fb42b4d76c2a15b41293be96ff0a84de6f830730
chaaeyoung <151603646+chaaeyoung@users.noreply.github.com>
2023-12-27 16:58:33 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    $("ul.menu li").hover(
+        function () {
+
+            $(".submenu, .bg").stop().fadeIn(600);
+        },
+        function () {
+
+            $(".submenu, .bg").stop().fadeOut(100);
+        }
+    );
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadeinBottom').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+            
+        }); 
+    });
+});
